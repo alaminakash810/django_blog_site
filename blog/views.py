7e022f3d1a9561d3b9ec66beb4f40fbce139f704
alaminakash810 <alaminakash810@gmail.com>
2025-08-03 15:21:01 +0600

    Add post publish and remove functionality
    Introduced views, URLs, and UI elements for publishing and removing blog posts. Added trash icon and corresponding button to post detail page, and implemented post_publish and post_remove views to handle these actions.

--- blog/views.py
+++ blog/views.py
@@ -44,3 +44,15 @@ def post_edit(request, pk):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.delete()
+    return redirect('post_list')
